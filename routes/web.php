0cb3492a5c20951e8f47047ff4cbbb88f39ef593
Alexander Yukal <alex.yu.dev@gmail.com>
2024-11-22 20:39:57 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,5 +25,21 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/genres', [GenreController::class, 'index'])->name('genres.index');
+Route::get('/genres/create', [GenreController::class, 'create'])->name('genres.create');
+Route::post('/genres', [GenreController::class, 'store'])->name('genres.store');
+Route::get('/genres/{genre}', [GenreController::class, 'show'])->name('genres.show');
+Route::get('/genres/{genre}/edit', [GenreController::class, 'edit'])->name('genres.edit');
+Route::put('/genres/{genre}', [GenreController::class, 'update'])->name('genres.update');
+Route::delete('/genres/{genre}', [GenreController::class, 'destroy'])->name('genres.destroy');
+
+Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/movies/create', [MovieController::class, 'create'])->name('movies.create');
+Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
+Route::get('/movies/{movie}', [MovieController::class, 'show'])->name('movies.show');
+Route::get('/movies/{movie}/edit', [MovieController::class, 'edit'])->name('movies.edit');
+Route::put('/movies/{movie}', [MovieController::class, 'update'])->name('movies.update');
+Route::delete('/movies/{movie}', [MovieController::class, 'destroy'])->name('movies.destroy');
 
 require __DIR__.'/auth.php';
