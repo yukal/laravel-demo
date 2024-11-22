4f8c486be1aa314498047bfd8bfe8fb9df6314a6
Alexander Yukal <alex.yu.dev@gmail.com>
2024-11-22 20:34:35 +0200

    add migrations

---
+++ database/migrations/2024_11_22_090420_create_genres_movies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('genres_movies', function (Blueprint $table) {
+            // $table->id();
+            $table->unsignedBigInteger('genre_id');
+            $table->unsignedBigInteger('movie_id');
+            $table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade');
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('genres_movies');
+    }
+};
