bbe278eaf54d1e5780cd2d4581e9ff304fa68834
Alexander Yukal <alex.yu.dev@gmail.com>
2024-11-28 17:15:29 +0200

    refactor: upgrade core dependencies

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,8 @@
+// Bootstrap framework
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+
+import axios from 'axios';
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
