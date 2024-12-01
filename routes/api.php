70a4fc2a981ced9525555437b93c94afbf1a3a52
Alexander Yukal <alex.yu.dev@gmail.com>
2024-12-01 04:20:06 +0200

    fix(api): route names

--- routes/api.php
+++ routes/api.php
@@ -11,14 +11,14 @@ Route::get('/user', function (Request $request) {
 
 // API v1
 
-Route::get('/v1/genres', [GenreController::class, 'index'])->name('genres.index');
-Route::post('/v1/genres', [GenreController::class, 'store'])->name('genres.store');
-Route::get('/v1/genres/{genre}', [GenreController::class, 'show'])->name('genres.show');
-Route::put('/v1/genres/{genre}', [GenreController::class, 'update'])->name('genres.update');
-Route::delete('/v1/genres/{genre}', [GenreController::class, 'destroy'])->name('genres.destroy');
+Route::get('/v1/genres', [GenreController::class, 'index'])->name('api.v1.genres.index');
+Route::post('/v1/genres', [GenreController::class, 'store'])->name('api.v1.genres.store');
+Route::get('/v1/genres/{genre}', [GenreController::class, 'show'])->name('api.v1.genres.show');
+Route::put('/v1/genres/{genre}', [GenreController::class, 'update'])->name('api.v1.genres.update');
+Route::delete('/v1/genres/{genre}', [GenreController::class, 'destroy'])->name('api.v1.genres.destroy');
 
-Route::get('/v1/movies', [MovieController::class, 'index'])->name('movies.index');
-Route::post('/v1/movies', [MovieController::class, 'store'])->name('movies.store');
-Route::get('/v1/movies/{movie}', [MovieController::class, 'show'])->name('movies.show');
-Route::put('/v1/movies/{movie}', [MovieController::class, 'update'])->name('movies.update');
-Route::delete('/v1/movies/{movie}', [MovieController::class, 'destroy'])->name('movies.destroy');
+Route::get('/v1/movies', [MovieController::class, 'index'])->name('api.v1.movies.index');
+Route::post('/v1/movies', [MovieController::class, 'store'])->name('api.v1.movies.store');
+Route::get('/v1/movies/{movie}', [MovieController::class, 'show'])->name('api.v1.movies.show');
+Route::put('/v1/movies/{movie}', [MovieController::class, 'update'])->name('api.v1.movies.update');
+Route::delete('/v1/movies/{movie}', [MovieController::class, 'destroy'])->name('api.v1.movies.destroy');
