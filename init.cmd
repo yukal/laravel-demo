7a0975ce04ca0841bb6f64b0a2fb482412c11dac
Alexander Yukal <alex.yu.dev@gmail.com>
2024-12-08 22:53:05 +0200

    devops: improve scripts

--- init.cmd
+++ init.cmd
@@ -1,15 +1,7 @@
 composer install
-
 php -r "file_exists('.env') || copy('.env.example', '.env');"
-php artisan storage:unlink
 php artisan storage:link
 php artisan key:generate
-php artisan migrate
-php artisan config:clear
-php artisan config:cache
-
-php artisan db:seed --class GenresSeeder
-php artisan db:seed --class MoviesSeeder
-php artisan db:seed --class GenresMoviesSeeder
+php artisan migrate --seed
 
 npm install
