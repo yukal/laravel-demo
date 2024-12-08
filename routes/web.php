dddcc014e53513b629b4e378b05b2132ab8fe9b0
Alexander Yukal <alex.yu.dev@gmail.com>
2024-12-08 22:46:45 +0200

    refactor: improve routes

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/genres', [GenreController::class, 'index'])->name('genres.index');
-Route::get('/genres/create', [GenreController::class, 'create'])->name('genres.create');
-Route::post('/genres', [GenreController::class, 'store'])->name('genres.store');
-Route::get('/genres/{genre}', [GenreController::class, 'show'])->name('genres.show');
-Route::get('/genres/{genre}/edit', [GenreController::class, 'edit'])->name('genres.edit');
-Route::put('/genres/{genre}', [GenreController::class, 'update'])->name('genres.update');
-Route::delete('/genres/{genre}', [GenreController::class, 'destroy'])->name('genres.destroy');
+Route::resource('genres', GenreController::class);
 
-Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
-Route::get('/movies/create', [MovieController::class, 'create'])->name('movies.create');
-Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
-Route::get('/movies/{movie}', [MovieController::class, 'show'])->name('movies.show');
-Route::get('/movies/{movie}/edit', [MovieController::class, 'edit'])->name('movies.edit');
-Route::put('/movies/{movie}', [MovieController::class, 'update'])->name('movies.update');
-Route::patch('/movies/{movie}', [MovieController::class, 'publish'])->name('movies.publish');
-Route::delete('/movies/{movie}', [MovieController::class, 'destroy'])->name('movies.destroy');
+Route::resource('movies', MovieController::class);
