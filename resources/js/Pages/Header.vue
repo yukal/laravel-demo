52d698c8421176b26add991d6f32f188c35aac43
Alexander Yukal <alex.yu.dev@gmail.com>
2024-12-12 21:53:22 +0200

    refactor(template): header optimization

--- resources/js/Pages/Header.vue
+++ resources/js/Pages/Header.vue
@@ -7,39 +7,36 @@ var showingNavigationDropdown = ref(false);
 </script>
 
 <template>
-    <header class="w-full mx-auto pt-8">
-        <nav class="w-full mx-auto sm:flex sm:items-center sm:justify-between">
-            <div class="flex items-center justify-between">
-                <Link href="/" class="flex-none text-xl font-semibold focus:outline-none focus:opacity-80" aria-label="TestTask">TestTask</Link>
+    <header class="header">
+        <nav class="navigation">
+            <div class="brand-wrap">
+                <Link href="/" class="brand" aria-label="TestTask">TestTask</Link>
 
-                <div class="sm:hidden">
-                    <button
-                        @click="showingNavigationDropdown = !showingNavigationDropdown"
-                        class="inline-flex items-center justify-center rounded-md p-2 text-gray-800 transition duration-150 ease-in-out border border-gray-200 hover:bg-white hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none dark:text-gray-500 dark:hover:bg-gray-900 dark:hover:text-gray-400 dark:focus:bg-gray-900 dark:focus:text-gray-400"
-                    >
-                        <UseIcon name="hamburger" size="24" :wstroke="2" class="h-6 w-6"
-                            :class="{
-                                hidden: showingNavigationDropdown,
-                                'inline-flex': !showingNavigationDropdown,
-                            }" />
+                <button @click="showingNavigationDropdown = !showingNavigationDropdown"
+                    class="btn-toggle-menu"
+                >
+                    <UseIcon name="hamburger" size="24" :wstroke="2"
+                        :class="{
+                            hidden: showingNavigationDropdown,
+                            'inline-flex': !showingNavigationDropdown,
+                        }" />
 
-                        <UseIcon name="x" size="24" :wstroke="2" class="h-6 w-6"
-                            :class="{
-                                hidden: !showingNavigationDropdown,
-                                'inline-flex': showingNavigationDropdown,
-                            }" />
+                    <UseIcon name="x" size="24" :wstroke="2"
+                        :class="{
+                            hidden: !showingNavigationDropdown,
+                            'inline-flex': showingNavigationDropdown,
+                        }" />
 
-                        <span class="sr-only">Toggle navigation</span>
-                    </button>
-                </div>
+                    <span class="sr-only">Toggle navigation</span>
+                </button>
             </div>
 
-            <div class="hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block" aria-labelledby="hs-navbar-example-collapse"
+            <div aria-labelledby="navbar-collapse"
                 :class="{
                     block: showingNavigationDropdown,
                     hidden: !showingNavigationDropdown,
                 }">
-                <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+                <div class="menu">
                     <slot name="navbar"></slot>
                 </div>
             </div>
